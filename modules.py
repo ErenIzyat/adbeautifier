6b33b2f53f41cfcbb3513f0405ddb6c7c82c623f
Eren Izyat <unforgiveneren@gmail.com>
2023-07-18 09:27:57 +0300

    Added ping_subdomains function

--- modules.py
+++ modules.py
@@ -9,3 +9,23 @@ def subdomain_discovery(domain):
     subfile=open("subds.txt","w")
     subfile.writelines(output.decode("utf-8"))
     subfile.close()
+#ping domains
+def ping_subdomains():
+    with open("subds.txt", "r") as subfile:
+        subdomains = subfile.read().splitlines()
+
+    subdomains_with_ip = []
+    with open("subswithip.txt", "w") as subswithip_file:
+        for subdomain in subdomains:
+            ping_command = f"dig +short {subdomain}"
+            try:
+                ping_output = subprocess.check_output(ping_command, shell=True)
+                ip_address = ping_output.decode('utf-8').strip()
+                if ip_address:
+                    subdomains_with_ip.append(f"{subdomain}: {ip_address}")
+                    subswithip_file.write(f"{subdomain}: {ip_address}\n")
+                    print(f"{subdomain} is reachable. Response: {ip_address}")
+                else:
+                    print(f"{subdomain} is not reachable.")
+            except subprocess.CalledProcessError as e:
+                print(f"{subdomain} is not reachable. Error: {e}")
